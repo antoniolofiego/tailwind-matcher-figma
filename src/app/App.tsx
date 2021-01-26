a8f0247d302da6e4c529148a0b967f4dbcaa8fc7
Antonio Lo Fiego <alofiego@hey.com>
2021-01-25 23:23:56 -0500

    Improve comments

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,32 @@
 import * as React from 'react';
-import './styles/ui.css';
-
-import {tailwindMatcher} from 'tailwind-matcher';
+import { tailwindMatcher } from 'tailwind-matcher';
 import Color from './components/Color';
+
+import './styles/ui.css';
 
 const colors = require('tailwindcss/colors');
 const flatten = require('flat');
 
-const tailwindReference = flatten(colors, {delimiter: '-'});
+// Flatten the Tailwind CSS color object for fast lookup
+const tailwindReference = flatten(colors, { delimiter: '-' });
 
 declare function require(path: string): any;
 
-const App = ({}) => {
-    const [actual, changeActual] = React.useState('');
-    const [tailwind, changeTailwind] = React.useState('');
+const App: React.FC = () => {
+    const [actual, changeActual] = React.useState(''); // Holds the Figma node color
+    const [tailwind, changeTailwind] = React.useState(''); // Holds the matched Tailwind color
 
     React.useEffect(() => {
         // Reads the message from the controller and updates on change
         window.onmessage = (event) => {
-            const {type, message} = event.data.pluginMessage;
+            // The UI will receive an array as a message from the controller
+            // This includes the type of message and the content of the message
+            // The type is either 'hex' (a valid color was found) or 'none' (no valid color was found)
+            const { type, message } = event.data.pluginMessage;
             if (type === 'hex') {
                 changeActual(message.toUpperCase());
                 changeTailwind(tailwindMatcher(message));
-            } else {
+            } else if (type === 'none') {
                 changeActual(message);
                 changeTailwind(message);
             }
